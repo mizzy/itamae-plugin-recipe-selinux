8b60b2536e93a9e80e450a90619d85c1dc2120e9
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-22 18:05:11 +0900

    Initial commit

---
+++ lib/itamae/plugin/recipe/selinux.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/selinux/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Selinux
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/selinux/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Selinux
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
